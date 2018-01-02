a249204cd6b40b1a438a031d75a306b1aa3dffdf
Vignesh Srinivasan <32835834+ThatIndianCoder@users.noreply.github.com>
2018-01-02 20:16:17 +0100

    Update GridLikeUniformStrategy.java

--- src/main/java/com/voronoiStrategy/playerone/GridLikeUniformStrategy.java
+++ src/main/java/com/voronoiStrategy/playerone/GridLikeUniformStrategy.java
@@ -1,10 +1,10 @@
 package com.voronoiStrategy.playerone;
 
 import com.model.Point;
-
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
+import java.lang.Math.*;
 
 public class GridLikeUniformStrategy extends PlayerOneStrategy {
 
@@ -12,15 +12,25 @@ public class GridLikeUniformStrategy extends PlayerOneStrategy {
         Random random = new Random();
         Set<Point> result = new HashSet<Point>();
 
-        // Calculate the sides of each grid to be formed
+        // Calculate the root of n. The sqrt_n should be an integer to form grids. 
+        float sqrt_n = sqrt(n);
 
-        float xdist = xmax / n;
-        float ydist = ymax / n;
+        // Calculate the size of each grid 
+        float xdist = xmax / sqrt_n;
+        float ydist = ymax / sqrt_n;
         float x = (xdist / 2);
         float y = (ydist / 2);
         
-        //Add the points to the Set result
-
+        //Adding the center points of all the grids to the Set 'result'
+        for (int i=0; i < sqrt_n; i++){
+            for (int j=0; j < sqrt_n; j++ ){
+                Point point = new Point(OWNER, x, y);
+                result.add(point);
+                x += xdist;
+            }
+            y += ydist;
+            x = (xdist / 2);
+        }
         if (ymax > xmax) { // If the bounding box is taller than it is wide, transform the points.
             return transformY(result, xmax, ymax);
         }
