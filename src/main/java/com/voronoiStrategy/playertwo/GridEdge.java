5fc42731fa594d936635f22ac39cd86d023da0b5
Vignesh Srinivasan <32835834+ThatIndianCoder@users.noreply.github.com>
2018-01-15 17:51:15 +0100

    Update GridEdge.java

--- src/main/java/com/voronoiStrategy/playertwo/GridEdge.java
+++ src/main/java/com/voronoiStrategy/playertwo/GridEdge.java
@@ -1,14 +1,18 @@
-import com.model;
+package com.voronoiStrategy.playertwo;
+
 
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
+
+import com.model.Point;
+
 import java.lang.Math.*;
 
-public class GridEdge implements Comparable {
+public class GridEdge implements Comparable<GridEdge> {
     public Point p1, p2;
     public float distance, midpoint;
-
+    private boolean flagX;
     public GridEdge(Point p1, Point p2) {
         this.p1 = p1;
         this.p2 = p2;
@@ -21,8 +25,16 @@ public class GridEdge implements Comparable {
     public Point getP2() {
         return p2;
     }
+    
+    public void setFlag(boolean value) {
+    	this.flagX = value;
+    }
+    
+    public boolean getFlag() {
+    	return flagX;
+    }
 
-    public getDistance(){
+    public float getDistance(){
     	float x1 = p1.getX();
     	float y1 = p1.getY();
     	float x2 = p2.getX();
@@ -37,15 +49,15 @@ public class GridEdge implements Comparable {
 
     }
 
-    public getMidpoint(){
+    public float getMidpoint(){
     	this.midpoint = (this.distance) / 2;
     	return midpoint;	
     }
+    
+	public int compareTo(GridEdge maxEdge) {
+		float comparedist= (float)((GridEdge)maxEdge).getDistance();
 
-    @Override
-    public int compareTo(GridEdge maxEdge) {
-        float comparedist=((Student)maxEdge).getDistance();
-
-        return (int)(comparedist-this.distance);
-    }
+        return (int) (comparedist-this.distance);
+		//return 0;
+	}
 }
